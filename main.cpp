d2cce5db5580fe2198b4bb6dbe7e491634469aa4
Liam Adams <liam1540@googlemail.com>
2023-01-27 10:58:06 -0500

    print outputs on new lines

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     for(double i=1; i<5; i++)
-        printf("square(%f)=%f, dsquare(%f)=%f", i, square(i), i, dsquare(i));
+        printf("square(%f)=%f, dsquare(%f)=%f \n", i, square(i), i, dsquare(i));
 }
 
 
